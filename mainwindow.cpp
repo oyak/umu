828ab67e468b74cc727428c336f39d645cb20092
veo_dev <veo86@bk.ru>
2019-04-29 18:48:06 +0300

    Improve GUI.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,21 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
-QString s0;
-QString s1;
-QString s2;
-QString s3;
-QString s4;
+    QString s0;
+    QString s1;
+    QString s2;
+    QString s3;
+    QString s4;
 
     ui->setupUi(this);
     pConfig = new CONFIG("");
 
     pDevice = new UNITLIN(pConfig);
-//
+    //
     drawCDULocalIPAddress();
     drawCDULocalPort();
     drawCDURemoteIPAddress();
@@ -27,36 +27,36 @@ QString s4;
 
     ui->lineEdit_27->setText(pDevice->getPathToObjectsFiles());
     ui->checkBox->setChecked(pDevice->getRestorePCConnectionFlagState());
-//
+    //
     ui->lineEdit_2->setEnabled(false);
     ui->lineEdit_3->setEnabled(false);
     ui->lineEdit_4->setEnabled(false);
     ui->lineEdit_5->setEnabled(false);
-//
+    //
     ui->lineEdit_18->setEnabled(false);
-//
+    //
     ui->lineEdit_10->setEnabled(false);
     ui->lineEdit_11->setEnabled(false);
     ui->lineEdit_12->setEnabled(false);
     ui->lineEdit_13->setEnabled(false);
 
     ui->lineEdit_19->setEnabled(false);
-//
+    //
     ui->lineEdit_14->setEnabled(false);
     ui->lineEdit_15->setEnabled(false);
     ui->lineEdit_16->setEnabled(false);
     ui->lineEdit_17->setEnabled(false);
-//
+    //
     ui->lineEdit_24->setEnabled(false);
-//
+    //
     ui->lineEdit_6->setEnabled(false);
     ui->lineEdit_7->setEnabled(false);
     ui->lineEdit_8->setEnabled(false);
     ui->lineEdit_9->setEnabled(false);
-//
+    //
     ui->lineEdit_25->setEnabled(false);
     ui->lineEdit_27->setEnabled(false);
-//
+    //
     ui->pushButton_2->setEnabled(false);
     ui->checkBox->setEnabled(false);
     ui->lineEdit->setEnabled(true);
@@ -71,52 +71,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_textChanged(const QString& arg1)
 {
-//
+    //
     pDevice->testPassword(arg1);
-//
+    //
     ui->lineEdit_2->setEnabled(true);
     ui->lineEdit_3->setEnabled(true);
     ui->lineEdit_4->setEnabled(true);
     ui->lineEdit_5->setEnabled(true);
-//
+    //
     ui->lineEdit_18->setEnabled(true);
-//
+    //
     ui->lineEdit_10->setEnabled(true);
     ui->lineEdit_11->setEnabled(true);
     ui->lineEdit_12->setEnabled(true);
     ui->lineEdit_13->setEnabled(true);
 
     ui->lineEdit_19->setEnabled(true);
-//
+    //
     ui->lineEdit_14->setEnabled(true);
     ui->lineEdit_15->setEnabled(true);
     ui->lineEdit_16->setEnabled(true);
     ui->lineEdit_17->setEnabled(true);
-//
+    //
     ui->lineEdit_24->setEnabled(true);
-//
+    //
     ui->lineEdit_6->setEnabled(true);
     ui->lineEdit_7->setEnabled(true);
     ui->lineEdit_8->setEnabled(true);
     ui->lineEdit_9->setEnabled(true);
-//
+    //
     ui->lineEdit_25->setEnabled(true);
     ui->lineEdit_27->setEnabled(true);
-//
+    //
     ui->pushButton_2->setEnabled(true);
     ui->checkBox->setEnabled(true);
-//
+    //
 }
 
 void MainWindow::drawCDULocalIPAddress()
 {
-QString s0;
-QString s1;
-QString s2;
-QString s3;
-QString s4;
+    QString s0;
+    QString s1;
+    QString s2;
+    QString s3;
+    QString s4;
     s0 = pDevice->getCDULocalIPAddress();
     pDevice->scatterIPAddress(s4, s3, s2, s1, s0);
     ui->lineEdit_2->setText(s4);
@@ -127,18 +127,18 @@ QString s4;
 
 void MainWindow::drawCDULocalPort()
 {
-QString s0;
+    QString s0;
     s0.setNum(pDevice->getCDULocalPort());
     ui->lineEdit_18->setText(s0);
 }
 
 void MainWindow::drawCDURemoteIPAddress()
 {
-QString s0;
-QString s1;
-QString s2;
-QString s3;
-QString s4;
+    QString s0;
+    QString s1;
+    QString s2;
+    QString s3;
+    QString s4;
 
     s0 = pDevice->getCDURemoteIPAddress();
     pDevice->scatterIPAddress(s4, s3, s2, s1, s0);
@@ -150,7 +150,7 @@ QString s4;
 //
 void MainWindow::drawCDURemotePort()
 {
-QString s0;
+    QString s0;
 
     s0.setNum(pDevice->getCDURemotePort());
     ui->lineEdit_19->setText(s0);
@@ -158,11 +158,11 @@ QString s0;
 
 void MainWindow::drawPCLocalIPAddress()
 {
-QString s0;
-QString s1;
-QString s2;
-QString s3;
-QString s4;
+    QString s0;
+    QString s1;
+    QString s2;
+    QString s3;
+    QString s4;
 
     s0 = pDevice->getPCLocalIPAddress();
     pDevice->scatterIPAddress(s4, s3, s2, s1, s0);
@@ -174,18 +174,18 @@ QString s4;
 //
 void MainWindow::drawPCLocalPort()
 {
-QString s0;
+    QString s0;
     s0.setNum(pConfig->getPCLocalPort());
     ui->lineEdit_24->setText(s0);
 }
 //
 void MainWindow::drawPCRemoteIPAddress()
 {
-QString s0;
-QString s1;
-QString s2;
-QString s3;
-QString s4;
+    QString s0;
+    QString s1;
+    QString s2;
+    QString s3;
+    QString s4;
 
     s0 = pDevice->getPCRemoteIPAddress();
     pDevice->scatterIPAddress(s4, s3, s2, s1, s0);
@@ -197,7 +197,7 @@ QString s4;
 //
 void MainWindow::drawPCRemotePort()
 {
-QString s0;
+    QString s0;
     s0.setNum(pDevice->getPCRemotePort());
     ui->lineEdit_25->setText(s0);
 }
@@ -244,17 +244,17 @@ bool MainWindow::testPCRemotePort()
     return pDevice->setCDULocalPort(ui->lineEdit_25->text());
 }
 
-void MainWindow::on_lineEdit_2_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_2_textChanged(const QString& arg1)
 {
     if (!testCDULocalIPAddress()) drawCDULocalIPAddress();
 }
 
-void MainWindow::on_lineEdit_3_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_3_textChanged(const QString& arg1)
 {
     if (!testCDULocalIPAddress()) drawCDULocalIPAddress();
 }
 
-void MainWindow::on_lineEdit_4_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_4_textChanged(const QString& arg1)
 {
     if (!testCDULocalIPAddress()) drawCDULocalIPAddress();
 }
@@ -264,22 +264,22 @@ void MainWindow::on_lineEdit_5_cursorPositionChanged(int arg1, int arg2)
     if (!testCDULocalIPAddress()) drawCDULocalIPAddress();
 }
 
-void MainWindow::on_lineEdit_18_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_18_textChanged(const QString& arg1)
 {
     if (!testCDULocalPort()) drawCDULocalPort();
 }
 
-void MainWindow::on_lineEdit_19_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_19_textChanged(const QString& arg1)
 {
     if (!testCDURemotePort()) drawCDURemotePort();
 }
 
-void MainWindow::on_lineEdit_24_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_24_textChanged(const QString& arg1)
 {
     if (!testPCLocalPort()) drawPCLocalPort();
 }
 
-void MainWindow::on_lineEdit_25_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_25_textChanged(const QString& arg1)
 {
     if (!testPCRemotePort()) drawPCRemotePort();
 }
@@ -290,16 +290,18 @@ void MainWindow::on_pushButton_2_released()
     pDevice->save();
 }
 
-void MainWindow::on_lineEdit_27_textChanged(const QString &arg1)
+void MainWindow::on_lineEdit_27_textChanged(const QString& arg1)
 {
     pDevice->setPathToObjectsFiles(arg1);
 }
 
 void MainWindow::on_checkBox_stateChanged(int arg1)
 {
-    if (arg1 == Qt::Checked)
-    {
+    if (arg1 == Qt::Checked) {
         pDevice->setRestorePCConnectionFlag(true);
     }
-        else pDevice->setRestorePCConnectionFlag(false);
+    else
+        pDevice->setRestorePCConnectionFlag(false);
 }
+
+void MainWindow::on_pushButton_released() {}
--- mainwindow.h
+++ mainwindow.h
@@ -5,7 +5,8 @@
 
 #include <QMainWindow>
 
-namespace Ui {
+namespace Ui
+{
 class MainWindow;
 }
 
@@ -14,7 +15,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget* parent = 0);
     ~MainWindow();
     void drawCDULocalIPAddress();
     void drawCDULocalPort();
@@ -36,34 +37,36 @@ public:
     bool testPCRemotePort();
 
 private slots:
-    void on_lineEdit_textChanged(const QString &arg1);
+    void on_lineEdit_textChanged(const QString& arg1);
 
-    void on_lineEdit_2_textChanged(const QString &arg1);
+    void on_lineEdit_2_textChanged(const QString& arg1);
 
-    void on_lineEdit_3_textChanged(const QString &arg1);
+    void on_lineEdit_3_textChanged(const QString& arg1);
 
-    void on_lineEdit_4_textChanged(const QString &arg1);
+    void on_lineEdit_4_textChanged(const QString& arg1);
 
     void on_lineEdit_5_cursorPositionChanged(int arg1, int arg2);
 
-    void on_lineEdit_18_textChanged(const QString &arg1);
+    void on_lineEdit_18_textChanged(const QString& arg1);
 
-    void on_lineEdit_19_textChanged(const QString &arg1);
+    void on_lineEdit_19_textChanged(const QString& arg1);
 
-    void on_lineEdit_24_textChanged(const QString &arg1);
+    void on_lineEdit_24_textChanged(const QString& arg1);
 
-    void on_lineEdit_25_textChanged(const QString &arg1);
+    void on_lineEdit_25_textChanged(const QString& arg1);
 
     void on_pushButton_2_released();
 
-    void on_lineEdit_27_textChanged(const QString &arg1);
+    void on_lineEdit_27_textChanged(const QString& arg1);
 
     void on_checkBox_stateChanged(int arg1);
 
+    void on_pushButton_released();
+
 private:
-    Ui::MainWindow *ui;
-    UNITLIN *pDevice;
-    CONFIG *pConfig;
+    Ui::MainWindow* ui;
+    UNITLIN* pDevice;
+    CONFIG* pConfig;
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
